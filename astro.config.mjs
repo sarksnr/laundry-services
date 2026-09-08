58a8a4b3849cf26fad16cf07029f32ab91fe889a
sarksnr <sreekumarsnambiar421@gmail.com>
2026-09-08 19:35:12 +0530

    Deploy Error Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sarksnr.github.io',
+  base: '/laundry-services',
   trailingSlash: 'always',
 
   vite: {
